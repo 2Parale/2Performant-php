0b2b1802d954a3706e4fd1f3ca380ca9eca15be6
RaduRaileanu <rraileanu@gmail.com>
2026-06-05 13:44:16 +0300

    Add validations for the Get Commissions endpoint

--- src/Filter/AdvertiserCommissionFilter.php
+++ src/Filter/AdvertiserCommissionFilter.php
@@ -5,7 +5,10 @@ namespace TPerformant\API\Filter;
 class AdvertiserCommissionFilter extends CommissionFilter {
     protected function filterableFields() {
         return array_merge(parent::filterableFields(), [
-            'transactionId' => 'transaction_id'
+            'transactionId' => 'transaction_id',
+            'startDate' => 'start_date',
+            'endDate' => 'end_date',
+            'type' => 'type',
         ]);
     }
 }
--- src/Filter/AdvertiserCommissionSort.php
+++ src/Filter/AdvertiserCommissionSort.php
@@ -3,4 +3,9 @@
 namespace TPerformant\API\Filter;
 
 class AdvertiserCommissionSort extends CommissionSort {
+    protected function sortableFields() {
+        return array_merge(parent::sortableFields(), [
+            'type' => 'type',
+        ]);
+    }
 }
